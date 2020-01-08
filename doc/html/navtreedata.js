7d8f75e37afb5632738364ac09096702728ea185
BAL API <balapi@broadcom.com>
2020-01-08 01:39:28 -0800

    BAL API

---
+++ doc/html/navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "BCM686xx Management API", "index.html", [
+    [ "BCM686xx API Concept", "index.html", [
+      [ "Introduction", "index.html#intro", null ],
+      [ "System Model", "index.html#object_model", null ],
+      [ "Object and Properties Implementation", "index.html#object_model_prop", [
+        [ "Object Structures", "index.html#object_model_structs", null ]
+      ] ]
+    ] ],
+    [ "Object Model", "object_model.html", "object_model" ],
+    [ "BCM686xx API Reference", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group__api.html",
+"group__object__model.html#ga502d59c4a6c977642bad5674cc4c0df3",
+"group__object__model.html#gacd0a38beb675efeed814b60f9eb0a70d",
+"group__object__model.html#gga10f671f92c80e918e0e53cdc0e2e2a23a98783e5792a2e8ea4cf84826ecae01db",
+"group__object__model.html#gga28274bc5d837bd7f29537f73fb491574ad69756b9c90b6c3f25b149f86d37565b",
+"group__object__model.html#gga4940c21f04c57d48905358867519af37ae362b4175aa8a20c7d54a68bd5e8038b",
+"group__object__model.html#gga625b53c2cac22ec1bb6595fa71dc0643af038d2e5bf3757d3248c1813caa01721",
+"group__object__model.html#gga7cc2b99c71fce9145141f1e8935ea067af1192b57cf0bec18b00cf55a295d4daf",
+"group__object__model.html#ggaa233555a1411b021f29c22c8a939ffd7a8e8a4480beaef191f3933e47dd49c2d2",
+"group__object__model.html#ggaa96eca84a0955824830c2718d6b1bb73af7b71b693d37e46bd13ca31014881cec",
+"group__object__model.html#ggabcc5f46ab7b5d1fbc0dda7dcf97b299da317a3979641b8e147c2991d23f50429c",
+"group__object__model.html#ggad569bc5310b78aa0d9dd007e4f06ee6aa1b6711db79e3258edd51e1b1808bb05e",
+"group__object__model.html#ggaf1384539c04aad38dfe1db64381b8ab9a80913b5851bba6ec1e3b542185ccd41c",
+"group__system__errno.html#ggae38e74508ba4aa92fe048a4aea02b9bfa352643a10a7954ae770169e98977092a",
+"structbcmolt__access__control__auto__cfg.html#ab0f1907eb36d7e51cfa2c70adc31a3df",
+"structbcmolt__device__connection__complete.html",
+"structbcmolt__eth__oam__set__mep__state__data.html",
+"structbcmolt__internal__nni__multi__cfg.html#ad663195fafb673c0c6dcc79aa64ec73e",
+"structbcmolt__lag__interface__state__update.html#acb74c28c266052f44b9a1bc2711f400c",
+"structbcmolt__olt__sw__error__data.html",
+"structbcmolt__onu__onu__activation__completed.html#a53a0520285dbcce8b1505cd6b6b5bb90",
+"structbcmolt__onu__xgpon__alarm.html#a5c83c739c54ff31d6c2202987970f617",
+"structbcmolt__pon__interface__protection__switching__switchover__completed.html#a3343ce1ba2d2723c71e12beb817b3506",
+"structbcmolt__tc__to__queue__key.html#a4fc3a0c58dfbd1e68224521185cb9384"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
